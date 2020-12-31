84cd81f7774a3e5000d1edf3eac5db0dd7d6cecc
alexibz <aborisenko@zendesk.com>
2020-12-31 09:28:13 -0500

    Updated all documents to v2

--- datepicker-react/src/components/DatePickerSimple/DatePickerSimple.js
+++ datepicker-react/src/components/DatePickerSimple/DatePickerSimple.js
@@ -1,70 +1,75 @@
-import React, { Component } from 'react';
-import DatePicker from 'react-datepicker'; // https://reactdatepicker.com/ for options and settings.
-import { Helmet } from 'react-helmet';
-import qs from 'qs';
-import moment from 'moment';
-import Button from  './Button';
-import 'react-datepicker/dist/react-datepicker.css'; // Base styling for Calendar
-import './DatePickerSimple.css'; // Custom Styling for Calendar
+import React, { Component } from "react";
+import DatePicker from "react-datepicker"; // https://reactdatepicker.com/ for options and settings.
+import { Helmet } from "react-helmet";
+import qs from "qs";
+import moment from "moment";
+import Button from "./Button";
+import "react-datepicker/dist/react-datepicker.css"; // Base styling for Calendar
+import "./DatePickerSimple.css"; // Custom Styling for Calendar
 
 class DatePickerSimple extends Component {
-    constructor() {
-        super();
-        const receivedParameters = qs.parse(window.location.search, { ignoreQueryPrefix: true });
+  constructor() {
+    super();
+    const receivedParameters = qs.parse(window.location.search, {
+      ignoreQueryPrefix: true,
+    });
 
-        this.state = {
-            selectedDate: moment(),
-            userId: receivedParameters.userId
-        }
-    }
-    componentDidMount() {
-        // webviewSdk script is added in index.html and initialized here.
-        // eslint-disable-next-line
-        window.webviewSdkInit;
-    }
-    handleChange = (date) => {
-        this.setState({
-            selectedDate: date
-        });
-    }
-    submitDate = () => {
-        const { server } = this.props;
-        const { selectedDate, userId } = this.state;
-        fetch(`${server}/date`, {
-            method: 'POST',
-            body: JSON.stringify(this.state),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-        .then(response => {
-            console.log(response, `Submitted ${selectedDate.format('MMM Do YYYY')} for user(${userId})`);
-            window.WebviewSdk.close();
-        })
-        .catch(err => console.log(err));
-    }
+    this.state = {
+      selectedDate: moment(),
+      userId: receivedParameters.userId,
+      conversationId: receivedParameters.conversationId,
+    };
+  }
+  componentDidMount() {
+    // webviewSdk script is added in index.html and initialized here.
+    // eslint-disable-next-line
+    window.webviewSdkInit;
+  }
+  handleChange = (date) => {
+    this.setState({
+      selectedDate: date,
+    });
+  };
+  submitDate = () => {
+    const { server } = this.props;
+    const { selectedDate, userId, conversationId } = this.state;
+    fetch(`${server}/date`, {
+      method: "POST",
+      body: JSON.stringify(this.state),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        console.log(
+          response,
+          `Submitted ${selectedDate.format(
+            "MMM Do YYYY"
+          )} for user(${userId}) in conversation(${conversationId})`
+        );
+        window.WebviewSdk.close();
+      })
+      .catch((err) => console.log(err));
+  };
 
-    render() {
-        const { selectedDate } = this.state;
+  render() {
+    const { selectedDate } = this.state;
 
-        return (
-            <div id="datepicker-simple">
-                <Helmet title={'Select Date'} />
-                <DatePicker
-                    inline
-                    minDate={moment()}
-                    selected={selectedDate}
-                    onChange={date => this.handleChange(date)}
-                />
-                <div className="nav">
-                    <Button
-                        selectedDate={selectedDate}
-                        submitDate={this.submitDate}
-                    />
-                </div>
-            </div>
-        )
-    }  
-};
+    return (
+      <div id="datepicker-simple">
+        <Helmet title={"Select Date"} />
+        <DatePicker
+          inline
+          minDate={moment()}
+          selected={selectedDate}
+          onChange={(date) => this.handleChange(date)}
+        />
+        <div className="nav">
+          <Button selectedDate={selectedDate} submitDate={this.submitDate} />
+        </div>
+      </div>
+    );
+  }
+}
 
 export default DatePickerSimple;
